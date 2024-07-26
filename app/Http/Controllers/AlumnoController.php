e4029cf0acb1893e59e9b6799f4c8d403070eb5d
Ceiber <ceiberconrado@gmail.com>
2024-07-26 17:07:32 -0500

    Alumnos y Cursos

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alumno;
 use Illuminate\Http\Request;
 
 class AlumnoController extends Controller
 {
-
+    public function index(Request $request){
+        if(!$request){
+            return response()->json('Not parameter domain');
+        }
+        $id = $request->query('dominio');
+        echo $id;
+        $carrera = Alumno::find($id);
+        if($carrera){
+            return response()->json($carrera);
+        }
+        return response()->json('Record not found', 404);
+    }
+    public function store(Request $request){
+        $this->validate($request, [
+            'codigo' => 'required|string|max:255',
+            'nombres' => 'required|string|max:255',
+            'dominio' => 'required|string|max:255',
+        ]);
+        $carrera = Alumno::create($request->all());
+        return response()->json($carrera, 201);
+    }
+    public function update($id, $dominio, Request $request){
+        $this->validate($request, [
+            'codigo' => 'required|string|max:255',
+            'nombres' => 'required|string|max:255',
+            'dominio' => 'required|string|max:255',
+        ]);
+        $carrera = Alumno::where('id', $id)->where('dominio', $dominio)->first();
+        if ($carrera) {
+            $carrera->update($request->all());
+            return response()->json($carrera, 201);
+        }
+        return response()->json('Record not found', 404);
+    }
+    public function destroy($id, $dominio)
+    {
+        $carrera = Alumno::where('id', $id)->where('dominio', $dominio)->first();
+        if ($carrera) {
+            $carrera->delete();
+            return response()->json('Record deleted', 201);
+        }
+        return response()->json('Record not found', 404);
+    }
+    public function show($id, $dominio){
+        $carrera = Alumno::where('id', $id)->where('dominio', $dominio)->first();
+        if ($carrera) {
+            return response()->json($carrera);
+        }
+        return response()->json('Record not found', 404);
+    }
 }
