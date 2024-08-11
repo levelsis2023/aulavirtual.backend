455c988e6d008916f6600a8fc560bedfdf1712f7
Yosel <yosel1989f@gmail.com>
2024-08-11 18:14:59 -0500

    convertir date a datetime en la consulta de horarios en las fecha inicio y fin de asistencia de curso

--- app/Http/Controllers/AsistenciaCursoController.php
+++ app/Http/Controllers/AsistenciaCursoController.php
@@ -11,7 +11,9 @@ class AsistenciaCursoController extends Controller
         $cursoId = $request->input('curso_id');
         $domainId = $request->input('domain_id');
         $fecha = $request->input('fecha');
-        
+
+//        return response()->json([]);
+
         $participantes = DB::table('curso_alumno as ca')
     ->select(
         'ca.curso_id',
@@ -29,11 +31,15 @@ class AsistenciaCursoController extends Controller
     ->where('ca.curso_id', $cursoId)
     ->where('ca.domain_id', $domainId)
     ->get();
-    
+
         $horarios = DB::table('curso_horario')
             ->where('curso_id',$cursoId)
             ->where('domain_id',$domainId)
-            ->select('day_id','fecha_inicio','fecha_fin')->get();
+            ->select(
+                'day_id',
+                DB::raw('TIMESTAMP(fecha_inicio) as fecha_inicio'),
+                DB::raw('TIMESTAMP(fecha_fin) as fecha_fin')
+            )->get();
         return response()->json(['participantes'=>$participantes,'horarios'=>$horarios]);
     }
     public function store(Request $request){
